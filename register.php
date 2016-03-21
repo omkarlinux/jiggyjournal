eec5cfa14b86424b4e6d1ddabfa375c4ff1edce4
omkardanke <omkardanke@gmail.com>
2016-03-21 17:54:41 -0600

    Register submit button

--- register.php
+++ register.php
@@ -111,7 +111,7 @@
                         <div id="success"></div>
                         <div class="row">
                             <div class="form-group col-xs-12">
-                                <input type="submit" name ="submit" class="btn btn-success btn-lg" value = "submit">
+                                <input type="submit" name ="submit" class="btn btn-success btn-lg" value = "Submit">
                             </div>
                         </div>
                     </form>
