525f42a87f067dc672d88b3f015a9a1180010c12
jiggyjournal <jiggyjournal@gmail.com>
2016-03-26 13:36:04 -0600

    del

--- ListView.php
+++ ListView.php
@@ -85,7 +85,8 @@ if(empty($_SESSION['userid']))
                                             </div>
                                             <div class="col-md-1">
 											<form action = "delete.php" method="post" >
-											<input type="image" src="img/deleteIcon.png" name="delete" value="<?php echo $journal_id;?>" >	
+											
+											<input type="image" src="img/deleteIcon.png" name="delete" value="<?php echo $journal_id;?>"  />	
 											</form>
 											</div>
                                             <div class="col-md-2 col-md-offset-0">
