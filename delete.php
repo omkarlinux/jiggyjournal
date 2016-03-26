9c7e6269f77bd8c1d9aca042caa52f74089aea82
Neethareddysama <sama.neethareddy@gmail.com>
2016-03-26 16:13:10 -0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -27,7 +27,7 @@
 	  die("Connection failed: " . $conn->connect_error);	
 	} 
 		$delete_id = $_POST['delete_id'];
-		$query = mysqli_query($conn, "DELETE FROM journal WHERE journal_id='delete_id' ");
+		$query = mysqli_query($conn, "DELETE FROM journal WHERE journal_id='$delete_id'");
     if($conn->query($query) == TRUE)
 	{ 
     ?>
