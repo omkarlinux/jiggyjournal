d06e47ccc7f8da7aca93ab07d64078214e345b35
shraddhabhandari <shraddhabhandari1@gmail.com>
2016-04-12 23:02:45 -0600

    Updated Password Recovery Page

--- PasswordRecovery.php
+++ PasswordRecovery.php
@@ -24,36 +24,82 @@
                         <div class="jumbotron">
                             <div class="about" id="PasswordRecovery">
                                   <div class="panel-group">
-                                    <div class="panel panel-success">
-                                        <div class="panel-heading text-left">
-                                              Forgot your Password? 
+                                   <div class="panel panel-success">
+                                        <div class="panel-heading">
+                                           &nbsp;
                                           </div>
                                         
                                         <div class="panel-body" style="margin:2px 15px 2px 15px !important;">
-                                            <div class="row">
+                                            <div id="show-email-row" class="row">
                                                 <div class="form-group">
-                                                    <div class="col-md-5 text-left">
-													    <label for="entryDate">Enter your Email Address:</label> <br /><br /><br />
-                                                        <label for="selectQuestion">Your Security Question is:</label> <br /><br /><br />
-                                                        <label for="addQuestion">Please Enter your Password</label> <br /><br /><br />
-                                                        
+                                                    <div class="col-md-2 text-left">
+                                                        <label for="entryDate">Enter Email ID:</label> <br /><br />
                                                     </div>
-                                                    <div class="col-md-7">
+                                                    <div class="col-md-7 col-md-offeset-2">
                                                         <input type="text" class="form-control input-sm" id="emailId" required><br />
-                                                        <select class="form-control btn-sm" id="selectedquestion">
-                                                            <option> What is your first pet's name?</option>
-                                                        </select>
-                                                        <br/>
-                                                       <input type="text" class="form-control input-sm" id="answer" required><br />
-                                                         <button class="btn btn-primary btn-sm text-left" type="submit" name="reset">Reset Password</button>&nbsp; &nbsp;&nbsp;&nbsp; 
-                                                         <button class="btn btn-primary btn-sm text-left" type="submit" name="cancel">Cancel</button>
+                                                   </div>
+                                                    <div class="col-md-1">
+                                                        <button class="btn btn-primary btn-sm" type="submit" name="go">Go</button>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                    
 
+                                            <div id="show-security-row">
+                                                <div class="row">
+                                                    <div class="col-md-2 text-left">
+                                                        <label for="selectQuestion">Security Question:</label> 
+
+                                                    </div>
+                                                    <div class="col-md-7 col-md-offeset-2">
+                                                        <span class="form-control disabled text-left input-sm">What is your first pet's name?</span>
+                                                    </div>
+                                                </div><br/>
+                                                
+                                                <div class="row">
+                                                    <div class="col-md-2 text-left">
+                                                       
+                                                        <label for="addQuestion">Enter your Answer:</label> 
+
+                                                    </div>
+                                                    <div class="col-md-7 col-md-offeset-2">
+                                                       
+                                                        <input type="text" class="form-control input-sm" id="answer" required>
+                                                    </div>
+                                                    <div class="col-md-1">
+                                                        <button class="btn btn-primary btn-sm text-left" type="submit" name="submit">Submit</button>
+                                                    </div>
+                                                </div>
+                                    </div>
+                                    <br/><br/>
+                                            <div id="show-reset-password">
+                                                <h4 class="text-left">Reset your password</h4>
+                                                <div class="row">
+                                                    <div class="col-md-2 text-left">
+                                                        <label>Enter Password:</label>
+
+                                                    </div>
+                                                    <div class="col-md-7 col-md-offeset-2">
+                                                        <input type="text" class="form-control input-sm" id="password" required>
+                                                    </div>
+                                                </div><br/>
+                                                <div class="row">
+                                                    <div class="col-md-2 text-left">
+
+                                                        <label>Confirm Password:</label>
+
+                                                    </div>
+                                                    <div class="col-md-7 col-md-offeset-2">
+
+                                                        <input type="text" class="form-control input-sm" id="confirmPassword" required>
+                                                    </div>
+                                                    <div class="col-md-1">
+                                                        <button class="btn btn-primary btn-sm text-left" type="submit" name="submit">Submit</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                </div>
+                            </div>
                                 </div>
                             </div>
                         </div>
