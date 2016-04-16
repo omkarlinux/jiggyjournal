293f7b0be68edd1337ccc94bfb5596c45d547b53
shraddhabhandari <shraddhabhandari1@gmail.com>
2016-04-16 12:25:08 -0600

    checking date in words
    using strtotime function

--- ListView.php
+++ ListView.php
@@ -98,7 +98,9 @@
 											</div>
 
                                             <div class="col-md-2 col-md-offset-0">
-                                                <?php echo $date; echo strptime($date,"m-d-Y"); echo date("F j, Y",strptime($date,"m-d-Y")); ?> 
+                                              <?php /*echo $date; echo strptime($date,"m-d-Y"); echo date("F j, Y",strptime($date,"m-d-Y")); */
+                                              echo $mydate = strtoTime($date); echo $printdate = date('F j, Y', $mydate);
+											  ?>
                                             </div>
                                             
                                         </div>
