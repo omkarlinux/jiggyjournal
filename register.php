29150c15b437cd8b4adde2344a99c5b2507b778c
jiggyjournal <jiggyjournal@gmail.com>
2016-04-26 09:38:10 -0600

    reg

--- register.php
+++ register.php
@@ -112,13 +112,15 @@
                             <div class="form-group col-xs-12 floating-label-form-group controls">
                                 <br/>
 								<form>
-								<input type="text" name="ques" id="ques" maxlength="255" style="width:50%"/>
+								<input type="text" name="ques" list="question" id="ques" value= maxlength="255" style="width:50%"/>
 								<select class="form-control btn-sm register-placeholder" name = "options" onchange="this.form.ques.value=this.options[this.selectedIndex].value" id="question">
+									<datalist id="question">  
 									<option value="">--Select a Security Question--</option>
 									<option value= "What is your first pet's name?">What is your first pet's name?</option>
 									<option value="What is your favorite color?">What is your favorite color?</option>
 									<option value="What is your favorite actor's name?">What is your favorite actor's name?</option>
 									<option value="Other">Other</option>
+									<datalist>
 								</select>
 								
                                 <p class="help-block text-danger"></p>
