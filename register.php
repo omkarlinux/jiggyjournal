bd607d4f359d2c8f3866e1164e63d988fee6d9ce
jiggyjournal <jiggyjournal@gmail.com>
2016-04-25 22:47:31 -0600

    reg

--- register.php
+++ register.php
@@ -42,7 +42,7 @@
 					$lname = $_POST["Lastname"];
 					$email = $_POST["email"];
 					$password = $_POST["password"];
-					$security_question = $_POST["question"];
+					$security_question = $_POST["ques"];
 					$answer = $_POST["answer"];
 					$sql1 = "SELECT * FROM user Where u_email='$email'";
 					$result = $conn->query($sql1);
@@ -111,14 +111,15 @@
 						<div class="row control-group">
                             <div class="form-group col-xs-12 floating-label-form-group controls">
                                 <br/>
+								<form>
 								<input type="text" name="ques" id="ques" maxlength="255" style="width:50%"/>
-                                <select class="form-control btn-sm register-placeholder" name = "question" onchange="this.form.ques.value=this.question[this.selectedIndex].value" id="question">
-                                    <option value="">--Select a Security Question--</option>
-                                    <option value= "What is your first pet's name?">What is your first pet's name?</option>
-                                    <option value="What is your favorite color?">What is your favorite color?</option>
-                                    <option value="What is your favorite actor's name?">What is your favorite actor's name?</option>
+								<select class="form-control btn-sm register-placeholder" name = "options" onchange="this.form.ques.value=this.options[this.selectedIndex].value" id="question">
+									<option value="">--Select a Security Question--</option>
+									<option value= "What is your first pet's name?">What is your first pet's name?</option>
+									<option value="What is your favorite color?">What is your favorite color?</option>
+									<option value="What is your favorite actor's name?">What is your favorite actor's name?</option>
 									<option value="Other">Other</option>
-                                </select>
+								</select>
 								
                                 <p class="help-block text-danger"></p>
                             </div>
