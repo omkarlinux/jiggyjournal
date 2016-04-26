1d6e1cfc74c1c9086fb6857621aa60490be12d8d
jiggyjournal <jiggyjournal@gmail.com>
2016-04-26 12:15:30 -0600

    reg

--- register.php
+++ register.php
@@ -54,6 +54,7 @@
 					 else
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 					 {
 					    $sql = "INSERT INTO user(u_fname,u_lname, u_email, password, security, answer) VALUES ('$fname','$lname' ,'$email','$password', '$security_question','$answer');";
 					
@@ -66,16 +67,10 @@
 					 	    echo "Not registered: " .$sql . "<br>" . $conn->error(); 
 					      }
 					    
-					   
-=======
 					 {	 
 					  $sql = "INSERT INTO user(u_fname,u_lname, u_email, password, security, answer) VALUES ('$fname','$lname' ,'$email','$password', '$security_question','$answer');";
 					
-=======
-					 {	 
-					  $sql = "INSERT INTO user(u_fname,u_lname, u_email, password, security, answer) VALUES ('$fname','$lname' ,'$email','$password', '$security_question','$answer');";
-					
->>>>>>> parent of 46e48a2... reg_paswords match issue
+
 					   if ($conn->query($sql) === TRUE) 
 					   {
 					 	 echo "Registered successfully";
@@ -84,10 +79,7 @@
 					   {
 					 	 echo "Not registered: " .$sql . "<br>" . $conn->error(); 
 					   }
-<<<<<<< HEAD
->>>>>>> parent of 46e48a2... reg_paswords match issue
-=======
->>>>>>> parent of 46e48a2... reg_paswords match issue
+
 					 }        
 				}
 			?>
