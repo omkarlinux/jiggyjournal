f2e36e6c77f0a5b9e45b976e53963a00f5caa65e
omkarlinux <omkardanke@gmail.com>
2016-04-25 20:47:37 -0600

    Form encoding changed

--- EditJournal.php
+++ EditJournal.php
@@ -56,7 +56,7 @@
                     <div class="intro-text">
                       <hr class="star-light">
                        <div class="jumbotron">
-                            <form name="editPageForm" id="editPageForm" method="post" >
+                            <form name="editPageForm" enctype="multipart/form-data" id="editPageForm" method="post" >
                                 <input type="hidden" name="action" value="<?php if(isset($_POST['edit'])){echo 'update';}
                                                                                 else{echo 'create';} ?>" />
                                 <div class="panel-group control-group">
